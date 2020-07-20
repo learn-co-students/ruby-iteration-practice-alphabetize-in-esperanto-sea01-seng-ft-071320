60c6ca60a99a78a323a48a736a976fa3e00741c2
afteralec <github email address>
2020-07-19 18:24:00 -0700

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,13 @@
-def alphabetize(arr)
-  # code here
+require 'pry'
+
+ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
+def alphabetize(phrases)
+  phrases.sort_by do |phrase|
+    phrase.chars.map.with_index do |char, i|
+      if char != " "
+        ESPERANTO_ALPHABET.chars.index(phrase[i])
+      end
+    end
+  end
 end
