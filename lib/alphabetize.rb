9de4527ff5cae77307b29e7ada9197e9dc181ded
afteralec <github email address>
2020-07-21 13:14:56 -0700

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,10 +4,6 @@ ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(phrases)
   phrases.sort_by do |phrase|
-    phrase.chars.map.with_index do |char, i|
-      if char != " "
-        ESPERANTO_ALPHABET.chars.index(phrase[i])
-      end
-    end
+    phrase.chars.map.with_index { |char, i| ESPERANTO_ALPHABET.chars.index(phrase[i]) }
   end
 end
